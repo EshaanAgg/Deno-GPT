ce2b657e2999b65224a674b15cadd2c7c37435cb
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-05-11 01:01:27 +0530

    Add testing script to make the requests

--- controllers/file_upload.ts
+++ controllers/file_upload.ts
@@ -1,10 +1,13 @@
 import { customContext } from "../types.ts";
 import { ADMIN_USER_IDS } from "../constants.ts";
-import { PDFDocument } from "https://cdn.skypack.dev/pdf-lib@^1.11.1?dts";
+
+const API_BASE_URL = "https://v2.convertapi.com/convert/pdf/to/txt";
 
 export const file_upload_handler = async (ctx: customContext) => {
   const file = await ctx.getFile();
-  const file_path = await file.download();
+  const download_file_url = `https://api.telegram.org/file/bot${Deno.env.get(
+    "TELEGRAM_BOT_TOKEN"
+  )}/${file.file_path}`;
 
   const userId = ctx.msg!.chat.id;
   if (!ADMIN_USER_IDS!.includes(userId.toString()))
@@ -17,25 +20,29 @@ export const file_upload_handler = async (ctx: customContext) => {
     "Thanks for the upload! We will try to parse it to generate all the questions! "
   );
 
-  if (path!.slice(-4) != ".pdf")
+  if (file.file_path!.slice(-4) != ".pdf")
     ctx.reply(
       "Currently we only support .pdf files for making questions! Please try again with the correct file type."
     );
 
-  console.log(file);
-  console.log(file_path);
+  const pdf_to_text_request = new Request(
+    `${API_BASE_URL}?Secret=${Deno.env.get(
+      "PDF_TO_TEXT_API_KEY"
+    )}&StoreFile=true&File=${download_file_url}`,
+    {
+      method: "POST",
+    }
+  );
 
-  const pdfData = await Deno.readFile(file_path);
-  const pdfDoc = await PDFDocument.load(pdfData);
-  const pages = await pdfDoc.getPages();
-  let textContent = "";
+  const pdf_to_text_response = await fetch(pdf_to_text_request);
+  const pdf_to_text_json = await pdf_to_text_response.json();
+  const text_url = pdf_to_text_json.Files.Url;
 
-  for (let i = 0; i < pages.length; i++) {
-    const page = pages[i];
-    const content = await page.getTextContent();
-    const strings = content.items.map((item) => item.str);
-    textContent += strings.join(" ");
-  }
+  const content_response = await fetch(text_url);
+  const content = await content_response.text();
 
-  ctx.reply(textContent);
+  await ctx.api.sendMessage(
+    userId,
+    "The content from the uploaded file has been successfully parsed. Generating questions now! "
+  );
 };
