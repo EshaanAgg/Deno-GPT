7a50e115c3f9e4bb9852c84bd76c02da78a54e98
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-05-10 16:54:02 +0530

    Debug file path

--- controllers/file_upload.ts
+++ controllers/file_upload.ts
@@ -2,10 +2,27 @@ import { customContext } from "../types.ts";
 import { ADMIN_USER_IDS } from "../constants.ts";
 import { PDFDocument } from "https://cdn.skypack.dev/pdf-lib@^1.11.1?dts";
 
+async function getNames(currentPath: string) {
+  const names: string[] = [];
+
+  for await (const dirEntry of Deno.readDir(currentPath)) {
+    const entryPath = `${currentPath}/${dirEntry.name}`;
+    names.push(entryPath);
+
+    if (dirEntry.isDirectory) {
+      names.push(await getNames(entryPath));
+    }
+  }
+
+  return names;
+}
+
 export const file_upload_handler = async (ctx: customContext) => {
   const file = await ctx.getFile();
   const path = file.file_path;
-
+  console.log(getNames("./"));
+  console.log(getNames("./../"));
+  console.log(getNames("./../../"));
   const userId = ctx.msg!.chat.id;
   if (!ADMIN_USER_IDS!.includes(userId.toString()))
     ctx.reply(
@@ -22,7 +39,7 @@ export const file_upload_handler = async (ctx: customContext) => {
       "Currently we only support .pdf files for making questions! Please try again with the correct file type."
     );
   console.log(file);
-  const pdfData = await Deno.readFile(`./${path!}`);
+  const pdfData = await Deno.readFile(`./../${path!}`);
   const pdfDoc = await PDFDocument.load(pdfData);
   const pages = await pdfDoc.getPages();
   let textContent = "";
