50c58c58af2516048208121c0b98ed43399f4e28
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-05-15 19:29:14 +0530

    Reduce logging

--- controllers/poll_creator.ts
+++ controllers/poll_creator.ts
@@ -58,56 +58,42 @@ export const pollCreator = async (id: number, ctx: customContext) => {
   const [question, qid, options, ans, ___, ____] = session[i];
   const ans_index = options.indexOf(ans);
 
-  try {
-    return_msg = await ctx.api.sendPoll(
-      id,
-      question,
-      // Implicitly convert the options to string type
-      options.map((opt: string | number) => opt.toString()),
-      {
-        is_anonymous: false,
-        type: "quiz",
-        correct_option_id: ans_index,
-        explanation: "Insert explanation here.",
-      }
-    );
-
-    console.log("Storing the information in session");
-
-    // Store the information to map the polls to the questions
-    ctx.session.pollInfo.push({
-      pollId: return_msg.poll.id,
-      questionId: qid,
-      correctAnsIndex: ans_index,
-    });
-
-    console.log("Stroed");
-    console.log(ctx.session.pollInfo);
-
-    // Check if the row corresponding to this question and user exists in the database
-    // and doing the necessary updates if not
-    const { data } = await supabase
-      .from("user_progress")
-      .select("*")
-      .eq("question", qid.toString())
-      .eq("user", id.toString());
-
-    console.log(data);
-    if (!data || data.length == 0) {
-      console.log("Inside no data");
-      const { data, error } = await supabase.from("user_progress").insert({
-        question: qid.toString(),
-        user: id.toString(),
-        correct: false,
-      });
-
-      console.log("Data", data);
-      console.log("Error", error);
+  return_msg = await ctx.api.sendPoll(
+    id,
+    question,
+    // Implicitly convert the options to string type
+    options.map((opt: string | number) => opt.toString()),
+    {
+      is_anonymous: false,
+      type: "quiz",
+      correct_option_id: ans_index,
+      explanation: "Insert explanation here.",
     }
-    console.log("Exit");
-  } catch (e) {
-    console.log(e);
+  );
+
+  // Store the information to map the polls to the questions
+  ctx.session.pollInfo.push({
+    pollId: return_msg.poll.id,
+    questionId: qid,
+    correctAnsIndex: ans_index,
+  });
+
+  // Check if the row corresponding to this question and user exists in the database
+  // and doing the necessary updates if not
+  const { data } = await supabase
+    .from("user_progress")
+    .select("*")
+    .eq("question", qid.toString())
+    .eq("user", id.toString());
+
+  if (!data || data.length == 0) {
+    await supabase.from("user_progress").insert({
+      question: qid.toString(),
+      user: id.toString(),
+      correct: false,
+    });
   }
+
   pbar_mid = pbar_msg?.message_id || 0;
   ctx.session.chatDescription = [
     i + 1,
@@ -115,8 +101,9 @@ export const pollCreator = async (id: number, ctx: customContext) => {
     session,
     return_msg?.message_id || 0,
     pbar_mid,
-    parseInt(return_msg?.poll?.id || "0"),
+    parseInt(return_msg!.poll.id),
   ];
+
   try {
     await supabase.from("TempMsgs").insert({
       user: id,
