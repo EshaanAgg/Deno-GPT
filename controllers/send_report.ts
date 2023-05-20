d436ab9e94b2006e8ccba8e646a3beea1a00275a
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-05-20 12:41:43 +0530

    Update report

--- controllers/send_report.ts
+++ controllers/send_report.ts
@@ -1,10 +1,27 @@
 import { customContext } from "../types.ts";
-import { get_decks_with_stats } from "./utilities.ts";
+import { DeckStatType, get_decks_with_stats } from "./utilities.ts";
+import { toTitleCase } from "../helper.ts";
 
 export const send_report = async (userId: string, ctx: customContext) => {
   const deckStats = await get_decks_with_stats(userId);
-  console.log(deckStats);
-  await ctx.api.sendMessage(userId, `Testing *markdown* features _yoo_`, {
-    parse_mode: "MarkdownV2",
+
+  let message =
+    "You have been amazing on the app! Here is a brief deck-wise summary of your preparation:\n\n";
+
+  deckStats.forEach((deck: DeckStatType) =>
+    message += `<u>${toTitleCase(deck.deck.replace("_", " "))}</u>
+<tg-spoiler>Accuracy: <b>${deck.accuracy}</b>
+Last Practiced: <b>${
+      deck.lastSolved == 10000
+        ? "NA"
+        : (deck.lastSolved.toString() + " days ago")
+    }</b>
+</tg-spoiler>
+`
+  );
+
+  console.log(message);
+  await ctx.api.sendMessage(userId, message, {
+    parse_mode: "HTML",
   });
 };
