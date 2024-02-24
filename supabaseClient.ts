3d228edd58780e32469f40fdec6d43c1646e3cb3
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2024-02-24 13:22:06 +0530

    fix: add persistSession = false in the Supabase config

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -2,6 +2,8 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const supabaseUrl = "https://gomhtyyltrqyoflqrgvy.supabase.co";
 const supabaseKey = Deno.env.get("DATABASE_KEY");
-const supabase = createClient(supabaseUrl, supabaseKey!);
+const supabase = createClient(supabaseUrl, supabaseKey!, {
+    auth: { persistSession: false }
+});
 
 export default supabase;
